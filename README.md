3251b031459d86c355441727b7ad50d7ad403d69
Welder Mileu <welderfelix21@gmail.com>
2019-09-04 21:11:32 -0300

    Em desenvolvimento

---
+++ README.md
@@ -0,0 +1,9 @@
+##Templates Parecido com youtube
+
+`Pendencias`
+
+- [ ] Passar os videos de forma dinamica
+- [ ] Colocar as informações em em json 
+	  para serem manipuladas
+- [ ] Arrumar a função do volume
+- [ ] Estilizar o range do volume
---
+++ main.js
@@ -0,0 +1,39 @@
+(function(){
+	const $ = (elem) => document.querySelector(elem);
+
+	const pause = () => {
+		if($("#video").paused){
+			$("#video").play();
+			$("#pause").innerHTML = "<i class='fa fa-pause'></i>";
+		}else{
+			$("#video").pause();
+			$("#pause").innerHTML = "<i class='fa fa-play'></i>";
+		}
+	}
+
+	const tempo = () => {
+		const loop = $("#video").currentTime / $("#video").duration;
+		
+		$(".transi").style.width = loop * 100 + "%";
+
+		if ($("#video").currentTime.toFixed(0) < 10) {
+			$(".init").innerHTML = `00:0${$("#video").currentTime.toFixed(0)}`;
+		}else{
+			$(".init").innerHTML = `00:${$("#video").currentTime.toFixed(0)}`;
+		}
+		
+		$(".fin").innerHTML = `00:${$("#video").duration.toFixed(0)}`;
+		
+		if ($("#video").ended) {
+			$("#pause").innerHTML = "<i class='fa fa-play'></i>";
+		} 
+	}
+
+	const volume = (e) => {
+		$("#video").volume = e.currentTarget.value / 100;
+	}
+
+	$("#pause").addEventListener("click", pause);
+	$("#video").addEventListener("timeupdate", tempo);
+	$("#vol").addEventListener("change", volume)
+})();
